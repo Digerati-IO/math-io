d2a134a94a98ea5e7444a69fed74f465a91a5f19
Rob Voss <rob.voss@digerati.io>
2016-09-28 10:41:57 -0700

    Initial commit, nothing to see yet.

--- content/index.php
+++ content/index.php
@@ -1,45 +1,30 @@
 <?php
+require '../vendor/autoload.php';
 
-/**
- * Step 1: Require the Slim Framework using Composer's autoloader
- *
- * If you are not using Composer, you need to load Slim Framework with your own
- * PSR-4 autoloader.
- */
-require 'vendor/autoload.php';
-
-/**
- * Step 2: Instantiate a Slim application
- *
- * This example instantiates a Slim application using
- * its default settings. However, you will usually configure
- * your Slim application now by passing an associative array
- * of setting names and values into the application constructor.
- */
+use Math\Algebra;
 $app = new Slim\App();
 
-/**
- * Step 3: Define the Slim application routes
- *
- * Here we define several Slim application routes that respond
- * to appropriate HTTP request methods. In this example, the second
- * argument for `Slim::get`, `Slim::post`, `Slim::put`, `Slim::patch`, and `Slim::delete`
- * is an anonymous function.
- */
 $app->get('/', function ($request, $response, $args) {
     $response->write("Welcome to Slim!");
     return $response;
 });
 
-$app->get('/hello[/{name}]', function ($request, $response, $args) {
-    $response->write("Hello, " . $args['name']);
-    return $response;
-})->setArgument('name', 'World!');
+$app->get('/math/gcd/{a}/{b}', function ($request, $response, $args) {
+    $response->write("GCD of " . $args['a'] . " and " . $args['b'] . " is: " . Algebra::gcd($args['a'], $args['b']));
 
-/**
- * Step 4: Run the Slim application
- *
- * This method should be called last. This executes the Slim application
- * and returns the HTTP response to the HTTP client.
- */
+    return $response;
+});
+
+$app->get('/math/lcm/{a}/{b}', function ($request, $response, $args) {
+    $response->write("LCM of " . $args['a'] . " and " . $args['b'] . " is: " . Algebra::lcm($args['a'], $args['b']));
+
+    return $response;
+});
+
+$app->get('/math/factors/{a}', function ($request, $response, $args) {
+    $response->write("Factors of " . $args['a'] . " are " . print_r(Algebra::factors($args['a'])));
+
+    return $response;
+});
+
 $app->run();
